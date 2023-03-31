7e73a884e239b779377150e05cbd578fed62f323
aljs2557 <jaila.357071@nv.ccsd.net>
2023-03-31 09:47:21 -0700

    added skybox and bullet disappears after a bit

--- Custom-Unity-Game-Project-main/Custom Unity Game Project/Assets/Scripts/WeaponSystem.cs
+++ Custom-Unity-Game-Project-main/Custom Unity Game Project/Assets/Scripts/WeaponSystem.cs
@@ -8,20 +8,18 @@ public class WeaponSystem : MonoBehaviour
     public GameObject player;
     public float bullet_speed;
     public float firing_speed;
-    public float max_bullet;
 
     bool cooldown = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0) && max_bullet > 0 && !cooldown)
+        if(Input.GetMouseButton(0) && !cooldown)
         {
             GameObject bullet_clone = Instantiate(bullet, transform.position, Quaternion.identity);
             bullet_clone.SetActive(true);
             Rigidbody rb = bullet_clone.GetComponent<Rigidbody>();
             rb.AddForce(player.transform.forward * bullet_speed * Time.deltaTime);
-            max_bullet -= 1;
             cooldown = true;
             StartCoroutine(cooldown_timer());
         }
